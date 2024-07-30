9afe1bacf97349d38010d95628d5ee47feea27f3
hiram_chavez <hiram.chavez@dataiq.com.mx>
2024-07-30 11:51:04 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Checkout from "./components/Checkout.tsx";
+
+function App() {
+
+  return (
+    <>
+        <h3>Checkout</h3>
+        <Checkout />
+    </>
+  )
+}
+
+export default App
